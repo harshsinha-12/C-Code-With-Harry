eedee9ff7bd607e92e7a35501ead657375b8f989
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-23 13:20:55 +0530

    sum

--- add.c
+++ add.c
@@ -3,12 +3,18 @@
 int main(int argc, char const *argv[])
 {
     int a, b;
-    printf("Enter number a: ");
+    printf(
+        "Enter number a: "
+        );
     scanf("%d", &a);
 
-    printf("Enter number b: ");
+    printf(
+        "Enter number b: "
+        );
     scanf("%d", &b);
 
-    printf("The sum of %d and %d is %d\n", a, b, a + b);
+    printf(
+        "The sum of %d and %d is %d\n", a, b, a + b
+        );
     return 0;
 }
