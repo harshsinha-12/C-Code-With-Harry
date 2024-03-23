b33f6538185e1da07a6c2c88d573e7887ecb578c
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-23 19:57:03 +0530

    logical

---
+++ bitwise.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 2; //10
+    int b = 3; //11
+    printf("a xor b = %d\n", a ^ b);
+}
+/* & Bitwise AND
+| Bitwise OR
+^ Bitwise XOR
+~ Bitwise Complement
+>> Shift Right Operator
+<< Shift Left Operator */
