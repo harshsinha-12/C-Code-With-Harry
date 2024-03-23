baa1a829481a1a02d77c3fb2de1c505a94037da4
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-23 20:29:11 +0530

    escape

---
+++ escape.c
@@ -0,0 +1,15 @@
+/* \t Inserts a tab space
+\b Inserts a backspace
+\n Inserts a new line
+\r Inserts a carriage return
+\f Inserts a form feed
+\’ Inserts a single quote character
+\’’Inserts a double quote character
+\\ Inserts a backslash character */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Printing inside a double quotation, \"Code With Harry\"");
+}
