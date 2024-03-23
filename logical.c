bb9a27e6c9dafd5f38b9c69234b13b369eedd725
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-23 19:54:46 +0530

    logical

---
+++ logical.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    int b = 0;
+    printf("a or b = %d\n", a || b);
+}
+// || is the logical OR operator. It returns true if either of the operands is true.
+// && is the logical AND operator. It returns true if both operands are true.
+// ! is the logical NOT operator. It returns true if the operand is false.
