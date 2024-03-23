df8642e2930ff375ae4278b3984be384c7320c83
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-24 01:08:26 +0530

    if-else

---
+++ if-else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 10;
+    if (num <= 10)
+    {
+        printf("Number is less than equal to 10.");
+    }
+    else
+    {
+        printf("Number is greater than 10.");
+    }
+    return 0;
+}
