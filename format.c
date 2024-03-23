a4042fb7265ff12ccd4ed658d46470124bbe33cd
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-23 20:19:32 +0530

    format

--- format.c
+++ format.c
@@ -5,3 +5,13 @@ int main()
     char c[100] = "CodeWithHarry";
     printf("Printing a string, %s.", c);
 }
+
+/* %c Used to print a character
+%d Used to print the signed integer
+%f Used to print the float values
+%i Used to print the unsigned integer
+%l Used to print the long integer
+%lf Used to print the double values
+%lu Used to print the unsigned integer or unsigned long integer
+%s Used to print the string
+%u Used to print the unsigned integer */
