a824438393ffa7e4988f5137825b2ba049fbf3e3
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-24 22:51:51 +0530

    def array

---
+++ defarray.c
@@ -0,0 +1,14 @@
+/* Array Operations
+Defining an array
+1. Without specifying the size of the array*/
+
+int arr[] = {1, 2, 3};
+/* Here, we can leave the square brackets empty, although the array cannot be left empty in this case. It must have elements in it. The size of the array is determined by the number of elements in the array. */
+
+
+
+/* 2. With specifying the size of the array*/
+
+int arr2[3] = {1, 2, 3};
+/* Here, we can specify the size of the array in its definition itself. We can then put array elements later as well. */
+
