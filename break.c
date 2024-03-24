ff9f56f102b8bb72625d19c5bd34564cc050887b
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-24 13:40:12 +0530

    break

---
+++ break.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int i = 0;
+    while (1)
+    {
+        if (i > 5)
+        {
+            break;
+        }
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
