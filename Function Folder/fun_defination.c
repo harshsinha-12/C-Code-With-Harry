f1899e39268dfb0105a324c0babd02372a2ccd98
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-25 01:36:29 +0530

    func defination

---
+++ Function Folder/fun_defination.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// Function declaration
+void func();
+
+int main()
+{
+    func(); // calling the function
+    return 0;
+}
+
+// Function definition
+void func()
+{
+    printf("This is the definition part.");
+}
