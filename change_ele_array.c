173a8ed8f5ff1a0cfde2ecafd23981ea8a0841e4
Harsh Sinha <sinha.harshsep@gmail.com>
2024-03-24 22:59:48 +0530

    changing element of array

---
+++ change_ele_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int arr[] = {1, 5, 7, 2};
+
+    arr[2] = 8; //changing the element on index 2
+
+    printf("%d ", arr[2]); //printing element on index 2
+    printf("%d ", arr); //printing array address
+    
+
+}
